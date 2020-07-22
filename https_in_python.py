ff13c1d0cf4a5c8bcb38c5134d40d877e00f7414
iamskab <iamsourav470@gmail.com>
2020-07-22 23:45:18 +0530

    HTTPS Code in python

---
+++ https_in_python.py
@@ -0,0 +1,12 @@
+
+# <--------------------- Python HTTPS Application ------------------------->
+# - Monitor and analyze network traffic
+# - Apply cryptography to keep data safe
+# - Describe the core concepts of Public Key Infrastructure (PKI)
+# - Create your own Certificate Authority
+# - Build a Python HTTPS application
+# - Identify common Python HTTPS warnings and errors
+
+
+# Your device and the server set up a TCP < Transmission control Protocol > connection.
+
